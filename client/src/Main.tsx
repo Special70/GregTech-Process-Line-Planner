49e64b47c74f5a7a3f3d65c919824cccfa6617ab
Josef Miko Urquico <josef.urquico@gmail.com>
2026-09-23 08:39:56 +0800

    use hashrouter

--- client/src/Main.tsx
+++ client/src/Main.tsx
@@ -7,28 +7,28 @@ import { AssetDataContextProvider } from './contexts/AssetDataContext.tsx'
 import { MaterialSuggesterDataProvider } from './contexts/MaterialSuggesterDataContext.tsx'
 import { ClientViewHandlerContextProvider } from './contexts/ClientViewHandlerContext.tsx'
 import { ReactFlowProvider } from '@xyflow/react'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { HashRouter, Route, Routes } from 'react-router'
 import PublishedGraphsBrowser from './PublishedGraphsBrowser.tsx'
 
 createRoot(document.getElementById('root')!).render(
 
   <StrictMode>
-    <BrowserRouter>
-    <ClientViewHandlerContextProvider>
-      <ReactFlowProvider>
-        <GraphDataContextProvider>
-          <MaterialSuggesterDataProvider>
-            <AssetDataContextProvider>
+    <HashRouter>
+      <ClientViewHandlerContextProvider>
+        <ReactFlowProvider>
+          <GraphDataContextProvider>
+            <MaterialSuggesterDataProvider>
+              <AssetDataContextProvider>
 
-              <Routes>
-                <Route path="/" element={<GraphRenderer />} />
-                <Route path="/published-graphs" element={<PublishedGraphsBrowser/>}/>
-              </Routes>
-            </AssetDataContextProvider>
-          </MaterialSuggesterDataProvider>
-        </GraphDataContextProvider>
-      </ReactFlowProvider>
-    </ClientViewHandlerContextProvider>
-    </BrowserRouter>
+                <Routes>
+                  <Route path="/" element={<GraphRenderer />} />
+                  <Route path="/published-graphs" element={<PublishedGraphsBrowser />} />
+                </Routes>
+              </AssetDataContextProvider>
+            </MaterialSuggesterDataProvider>
+          </GraphDataContextProvider>
+        </ReactFlowProvider>
+      </ClientViewHandlerContextProvider>
+    </HashRouter>
   </StrictMode>,
 )
